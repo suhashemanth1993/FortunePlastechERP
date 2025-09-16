134742ef4aa10575bc83b39825bdc1f550d82ea6
suhashemanth <suhas@fortuneplastech.com>
2025-09-16 22:55:43 +0530

    This is my first commit

---
+++ src/main/java/com/fortune/fortuneplastecherp/FortunePlastechErpApplication.java
@@ -0,0 +1,13 @@
+package com.fortune.fortuneplastecherp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunePlastechErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortunePlastechErpApplication.class, args);
+    }
+
+}
